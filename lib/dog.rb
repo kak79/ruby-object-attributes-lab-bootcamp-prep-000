71ee1178d668c4d5be9b91c4692391911ea32dc8
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-29 11:17:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,17 @@
 class Dog 
   
- def initialize(name)
-   @name = name
- end
+  def initialize(name)
+    @name = name
+  end
  
+  def name
+    @name 
+  end
  
-  
+  def name=(new_dog)
+    @name = new_dog
+  end
+
   
   
   
