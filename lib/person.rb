8d97f11e7bf8ee4afdf00191b74a98514d6eb569
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-29 11:29:14 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,19 @@
 class Person 
   
-  def name()
-  
-  
-  
+  def name
+    @name 
+  end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def job 
+    @job  
+  end
+ 
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+ 
 end
